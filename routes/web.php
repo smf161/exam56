c3f5d45b941b4e15040996746c1b51b0d5e31771
smf161 <34880443+smf161@users.noreply.github.com>
2018-08-06 14:52:01 +0800

    從路由送參數給視圖

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,32 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
+//第一種
+// Route::get('/', function () {
+//     return view('welcome')
+//         ->with('name', 'tad')
+//         ->with('say', '嗨！');
+// });
+
+//第二種
+// Route::get('/', function () {
+//     $data = ['name' => 'tad', 'say' => '~hi~'];
+//     return view('welcome', $data);
+// });
+
+//第三種
+// Route::get('/', function () {
+//     return view('welcome',  ['name' => 'tad', 'say' => '~hi~']);
+// });
+
+//第四種
 Route::get('/', function () {
-    return view('welcome');
+    $name = 'tad';
+    $say  = '~hi~';
+
+    return view('welcome', compact('name', 'say'));
 });
 
 Auth::routes();
